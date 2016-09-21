5a30a296de73e052e541e1a3035d4cfc82e52db4
Alexander Cristofaro <acristo@broadinstitute.org>
2016-09-21 17:33:05 -0400

    endcheck should be completely fixed

--- type2_endcheck.py
+++ type2_endcheck.py
@@ -24,25 +24,19 @@ def main(ends_list):
     sim_list = set([])
     rc_list = [revcomplement(x) for x in ends_list]
     for x in ends_list:
-        for g in ends_list:
-            if g != x:
-                selfscore = align(x, g)
-                if selfscore.size >= 3:
-                    revset = [g, reverse_region(g)]
-                    for k in revset:
-                        positionscore = align(x, k[:3])
-                    if positionscore.size == 2:
-                        notgood = True
-                        sim_list.add((x, g))
+        if x in rc_list:
+            notgood = True
+            idx = rc_list.index(x)
+            sim_list.add((x, rc_list[idx]))
         for h in rc_list:
             revscore = align(x, h)
-            if revscore.size >= 3:
+            if revscore >= 3:
                 rrevset = [h, reverse_region(h)]
                 for p in rrevset:
-                    rpositionscore = align(x, p[:3])
-                    if rpositionscore.size == 2:
+                    rpositionscore = align(x[:2], p[:2])
+                    if rpositionscore == 2:
                         notgood = True
-                        sim_list.add((x, h))
+                        sim_list.add((x, p))
     if not notgood:
         print('Good to go!!!')
         if silly_list:
@@ -52,7 +46,7 @@ def main(ends_list):
         if silly_list:
             print 'Bad entry: ', silly_list
         for x in sim_list:
-            print 'Entry: ' + str(x[0]) + ' > ' + str(x[1]) + '(' + revcomplement(x[1]) + ')'
+            print 'Entry: ' + str(x[0]) + ' > (' + revcomplement(x[0]) + ') : ' + revcomplement(x[1]) + ' >  (' + reverse_region(x[1]) + ')'
 
 
 revcomplement_memo = {'A':'T'}
@@ -80,8 +74,14 @@ def reverse_region(region):
 
 
 def align(end, rcend):
-    matcher = difflib.SequenceMatcher(a = end, b = rcend)
-    match_score = matcher.find_longest_match(0, len(matcher.a), 0, len(matcher.b))
+    #matcher = difflib.SequenceMatcher(a = end, b = rcend)
+    #match_score = matcher.find_longest_match(0, len(matcher.a), 0, len(matcher.b))
+    #match_score = matcher.find_longest_match(0, len(matcher.a), 0, len(matcher.b))
+    pairs = zip(end,rcend)
+    match_score = 0
+    for a, b in pairs:
+        if a == b:
+            match_score += 1
     return match_score
 
 
