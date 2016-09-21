ec7c23f3c5ecea444f8f74dc8617c921bccbe4c3
Alexander Cristofaro <acristo@broadinstitute.org>
2016-09-21 16:05:37 -0400

    modification of selection criteria to 3 total match filter followed by position-selective match

--- type2_endcheck.py
+++ type2_endcheck.py
@@ -1,13 +1,66 @@
+"""
+Module to check whether TypeIIs end-sites are compatible. Looks for
+greater than 2 basepair similarity for each input sequence given.
+Comparisons are made between each element in the list and to the reverse
+complement of each element.
+"""
 import sys
 import difflib
 from string import maketrans
 
+
+def main(ends_list):
+    ends_list = ends_list.split(",")
+    ends_list = [x.strip(" ") for x in ends_list]
+    ncs = 'ATGC'
+    silly_list = []
+    for end in ends_list:
+        for c in end:
+            if c not in ncs:
+                ter = ends_list.index(end)
+                silly_list = ends_list.pop(ter)
+                break
+    notgood = False
+    sim_list = set([])
+    rc_list = [revcomplement(x) for x in ends_list]
+    for x in ends_list:
+        for g in ends_list:
+            if g != x:
+                selfscore = align(x, g)
+                if selfscore.size >= 3:
+                    revset = [g, reverse_region(g)]
+                    for k in revset:
+                        positionscore = align(x, k[:3])
+                    if positionscore.size == 2:
+                        notgood = True
+                        sim_list.add((x, g))
+        for h in rc_list:
+            revscore = align(x, h)
+            if revscore.size >= 3:
+                rrevset = [h, reverse_region(h)]
+                for p in rrevset:
+                    rpositionscore = align(x, p[:3])
+                    if rpositionscore.size == 2:
+                        notgood = True
+                        sim_list.add((x, h))
+    if not notgood:
+        print('Good to go!!!')
+        if silly_list:
+            print 'Bad entry: ', silly_list
+    else:
+        print('Not good!')
+        if silly_list:
+            print 'Bad entry: ', silly_list
+        for x in sim_list:
+            print 'Entry: ' + str(x[0]) + ' > ' + str(x[1]) + '(' + revcomplement(x[1]) + ')'
+
+
 revcomplement_memo = {'A':'T'}
-revcompTBL = maketrans("AGCTagctWSKMYRnN", "TCGAtcgaWSMKTYnN")
+revcompTBL = maketrans('AGCTagctWSKMYRnN', 'TCGAtcgaWSMKTYnN')
 def revcomplement(seq):
     """
     revcomplement(seq)
-    A quick reverse-complement routine that memo-izes queries, understands
+    A quick reverse-complement routine that understands
     IUPAC ambiguity codes, and preserves case.
     """
     global revcomplement_memo
@@ -22,44 +75,15 @@ def revcomplement(seq):
     return(rc)
 
 
+def reverse_region(region):
+    return region[::-1]
+
+
 def align(end, rcend):
     matcher = difflib.SequenceMatcher(a = end, b = rcend)
     match_score = matcher.find_longest_match(0, len(matcher.a), 0, len(matcher.b))
     return match_score
 
-
-def main(ends_list):
-    ends_list = ends_list.split(",")
-    ends_list = [x.strip(" ") for x in ends_list]
-    ncs = 'ATGC'
-    rc_list = []
-    silly_list = []
-    for end in ends_list:
-        for c in end:
-            if c not in ncs:
-                ter = ends_list.index(end)
-                silly_list = ends_list.pop(ter)
-                break
-
-    notgood = False
-    sim_list = []
-    rc_list = [revcomplement(x) for x in ends_list]
-    for x in ends_list:
-        for d in rc_list:
-            score = align(x, d)
-            if score.size >= 3:
-                notgood = True
-                sim_list.append((x, revcomplement(d)))
-    if not notgood:            
-        print("Good to go!!!")
-        if silly_list:
-            print "Bad entry: ", silly_list
-    else:
-        print("Not good!")
-        if silly_list:
-            print "Bad entry: ", silly_list
-        for x in sim_list:
-            print(x)
 
 if __name__ == "__main__":
     main(sys.argv[1])
