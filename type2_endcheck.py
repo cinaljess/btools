f50f6af56efb87a36dea76e212ae59eecba5c79d
Alexander Cristofaro <acristo@broadinstitute.org>
2016-09-23 12:11:09 -0400

    all cases should be caught now

--- type2_endcheck.py
+++ type2_endcheck.py
@@ -1,12 +1,16 @@
 """
 Module to check whether TypeIIs end-sites are compatible. Looks for
-greater than 2 basepair similarity for each input sequence given.
+3bp homology then 2 basepair edge-homology for each input sequence given.
 Comparisons are made between each element in the list and to the reverse
-complement of each element.
+complement of each element. Repeat elements are also validated along with
+noncanonical basepairs.
+
+Example:
+    python2.7 type2_endcheck.py "GAGG, GAGG, TACT, GACT, PAPP"
 """
 import sys
-import difflib
 from string import maketrans
+from collections import Counter
 
 
 def main(ends_list):
@@ -23,11 +27,27 @@ def main(ends_list):
     notgood = False
     sim_list = set([])
     rc_list = [revcomplement(x) for x in ends_list]
+    counts = Counter(ends_list)
+    self_list = set([])
+    # Check list for repeats
+    for c, n in counts.items():
+        if n >= 2:
+            notgood = True
+            self_list.add((c))
     for x in ends_list:
+        # Validate no ends share homology to each other
+        for g in ends_list:
+            if g != x:
+                score = align(x, g)
+                if score >= 3:
+                    notgood = True
+                    sim_list.add((x, g))
+        # Validate no reverse complements are equivalent to entry list
         if x in rc_list:
             notgood = True
             idx = rc_list.index(x)
             sim_list.add((x, rc_list[idx]))
+        # Validate no ends share 3 max homology & 2bp edge homology of revers complement list
         for h in rc_list:
             revscore = align(x, h)
             if revscore >= 3:
@@ -46,27 +66,22 @@ def main(ends_list):
         if silly_list:
             print 'Bad entry: ', silly_list
         for x in sim_list:
-            print 'Entry: ' + str(x[0]) + ' > (' + revcomplement(x[0]) + ') : ' + revcomplement(x[1]) + ' >  (' + reverse_region(x[1]) + ')'
+            print 'Entry: ' + str(x[0]) + ' > (' + revcomplement(x[0]) + ') : ' + revcomplement(x[1]) + ' >  (' + \
+                  reverse_region(x[1]) + ')'
+        for x in self_list:
+            print 'Entry: ' + x + ' appeared more than once'
 
 
-revcomplement_memo = {'A':'T'}
-revcompTBL = maketrans('AGCTagctWSKMYRnN', 'TCGAtcgaWSMKTYnN')
 def revcomplement(seq):
     """
-    revcomplement(seq)
     A quick reverse-complement routine that understands
     IUPAC ambiguity codes, and preserves case.
     """
-    global revcomplement_memo
-    try:
-        rc = revcomplement_memo[seq]
-    except KeyError:
-        _t = list(seq.translate(revcompTBL))
-        _t.reverse()
-        rc = ''.join(_t)
-        revcomplement_memo[seq] = rc
-        revcomplement_memo[rc]  = seq
-    return(rc)
+    revcompTBL = maketrans('AGCTagctWSKMYRnN', 'TCGAtcgaWSMKTYnN')
+    _t = list(seq.translate(revcompTBL))
+    _t.reverse()
+    rc = ''.join(_t)
+    return rc
 
 
 def reverse_region(region):
@@ -74,10 +89,7 @@ def reverse_region(region):
 
 
 def align(end, rcend):
-    #matcher = difflib.SequenceMatcher(a = end, b = rcend)
-    #match_score = matcher.find_longest_match(0, len(matcher.a), 0, len(matcher.b))
-    #match_score = matcher.find_longest_match(0, len(matcher.a), 0, len(matcher.b))
-    pairs = zip(end,rcend)
+    pairs = zip(end, rcend)
     match_score = 0
     for a, b in pairs:
         if a == b:
